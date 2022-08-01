ca6e5f9031d6a474625712244fbaf270b2458e0e
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2022-08-01 18:23:40 -0300

    Adiciona meus primeiros testes unitários.

--- src/main/kotlin/StringFilter.kt
+++ src/main/kotlin/StringFilter.kt
@@ -1,2 +1,11 @@
 class StringFilter {
+    fun containsInString(str: String): Boolean {
+        return str.contains("%")
+    }
+    fun filterNumber(str: String, number: Int): List<String> {
+        return str
+            .split(",")
+            .filter { Integer.parseInt(it) > number }
+            .map { it }
+    }
 }
--- src/test/kotlin/StringFilterTest.kt
+++ src/test/kotlin/StringFilterTest.kt
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class StringFilterTest {
+    @Test
+    fun containsInString() {
+        val stringFilter = StringFilter()
+        Assertions.assertEquals(true, stringFilter.containsInString("njkjlnajlnckslanckslan%nklnklnklnkl"))
+    }
+
+    /**
+     * Deve retornar uma lista com apenas números maiores que 100.
+     */
+    @Test
+    fun filterNumber() {
+        val stringFilter = StringFilter()
+        Assertions.assertEquals(listOf("1001", "200", "187"), stringFilter.filterNumber("1001,87,200,99,187,55", 100))
+    }
 }
